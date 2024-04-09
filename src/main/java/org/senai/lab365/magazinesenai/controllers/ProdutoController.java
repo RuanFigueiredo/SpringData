66daf0ba07c50141acbac30481de47dca1a8248c
ruanthiago67@gmail.com <ruanthiago67@gmail.com>
2024-04-09 09:58:50 -0300

    iniciando api e injetando dependências

---
+++ src/main/java/org/senai/lab365/magazinesenai/controllers/ProdutoController.java
@@ -0,0 +1,7 @@
+package org.senai.lab365.magazinesenai.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProdutoController {
+}
---
+++ src/main/java/org/senai/lab365/magazinesenai/services/ProdutoService.java
@@ -0,0 +1,15 @@
+package org.senai.lab365.magazinesenai.services;
+
+
+import org.senai.lab365.magazinesenai.models.Produto;
+import org.senai.lab365.magazinesenai.repositories.ProdutoRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProdutoService {
+    private ProdutoRepository repository;
+
+    public ProdutoService(ProdutoRepository repository) {
+        this.repository = repository;
+    }
+}
