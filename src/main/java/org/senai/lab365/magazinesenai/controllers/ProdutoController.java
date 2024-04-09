4aba258d024382af3dc9e7e57b837e7b3f751268
ruanthiago67@gmail.com <ruanthiago67@gmail.com>
2024-04-09 14:49:30 -0300

    Adicionando + endpoints( crud)

--- src/main/java/org/senai/lab365/magazinesenai/controllers/ProdutoController.java
+++ src/main/java/org/senai/lab365/magazinesenai/controllers/ProdutoController.java
@@ -3,10 +3,9 @@ package org.senai.lab365.magazinesenai.controllers;
 import org.senai.lab365.magazinesenai.models.Produto;
 import org.senai.lab365.magazinesenai.services.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
@@ -17,8 +16,29 @@ public class ProdutoController {
     @Autowired
     ProdutoService service;
 
+
     @PostMapping
     public void cadastrar (@RequestBody Produto produto){
         service.salvar(produto);
+    }
+
+    @PutMapping
+    public void atualizar(@RequestBody Produto produto){
+        service.salvar(produto);
+    }
+
+    @DeleteMapping
+    public void excluir(@RequestBody Produto produto){
+        service.excluir(produto);
+    }
+
+    @GetMapping("/lista")
+    public List<Produto> listaTodos(){
+        return service.buscaTodos();
+    }
+
+    @GetMapping("{id}")
+    public Produto buscaPorId(@PathVariable Long id){
+        service.buscaPorId(id);
     }
 }
--- src/main/java/org/senai/lab365/magazinesenai/services/ProdutoService.java
+++ src/main/java/org/senai/lab365/magazinesenai/services/ProdutoService.java
@@ -5,6 +5,8 @@ import org.senai.lab365.magazinesenai.models.Produto;
 import org.senai.lab365.magazinesenai.repositories.ProdutoRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProdutoService {
     private ProdutoRepository repository;
@@ -17,5 +19,17 @@ public class ProdutoService {
     //invocando o save do repository
     public void salvar(Produto produto){
         repository.save(produto);
+    }
+
+    public void excluir(Produto produto){
+        repository.delete(produto);
+    }
+
+    public List<Produto> buscaTodos(){
+        return (List<Produto>) repository.findAll();
+    }
+
+    public Produto buscaPorId(Long id){
+        return repository.findById(id).get();
     }
 }
